5e4bd16ecaaf8d02a1745f9ded6d8bd233a4b4b6
Shu-ri <des2295@gmail.com>
2016-07-26 01:24:58 +0900

    0725 board, member 쿼리 추가
    근데 멤버DB에 이름과 우편번호가 없다..

---
+++ SpringProject_YDCAT/src/main/java/model/board/BoardDto.java
@@ -0,0 +1,103 @@
+package model.board;
+
+import java.util.Date;
+
+public class BoardDto {
+	
+	//º¯¼ö
+	private int no;
+	private String id;
+	private String subject;
+	private String content;
+	
+	private Date regdate;
+	private int readcount;
+	private int ref;
+	private int ref_step;
+	private int ref_level;
+	
+	private String ip;
+
+	
+	//getter setter
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+	public int getReadcount() {
+		return readcount;
+	}
+
+	public void setReadcount(int readcount) {
+		this.readcount = readcount;
+	}
+
+	public int getRef() {
+		return ref;
+	}
+
+	public void setRef(int ref) {
+		this.ref = ref;
+	}
+
+	public int getRef_step() {
+		return ref_step;
+	}
+
+	public void setRef_step(int ref_step) {
+		this.ref_step = ref_step;
+	}
+
+	public int getRef_level() {
+		return ref_level;
+	}
+
+	public void setRef_level(int ref_level) {
+		this.ref_level = ref_level;
+	}
+
+	public String getIp() {
+		return ip;
+	}
+
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+		
+}//class end

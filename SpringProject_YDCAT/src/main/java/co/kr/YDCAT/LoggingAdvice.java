1109ea5572d834f8f2fa9715a651b0a51a3aa2fa
한창민 <enigmatic_duke@naver.com>
2016-07-27 21:52:22 +0900

    AOP 성공, 자세한 공부는 계속 된다.
    1.POM.XML에 메이븐 의존 주입 3개정도 했음. 최하단 위치
    2. aspect 부분에서 execution 정규화 손봤음.
    3. auto proxy 설정했음. aspect 빈즈를 생성해줬음

--- SpringProject_YDCAT/src/main/java/co/kr/YDCAT/LoggingAdvice.java
+++ SpringProject_YDCAT/src/main/java/co/kr/YDCAT/LoggingAdvice.java
@@ -1,17 +1,18 @@
 package co.kr.YDCAT;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect; 
 import org.aspectj.lang.annotation.Before; 
-import org.aspectj.lang.annotation.Pointcut; 
- 
+import org.aspectj.lang.annotation.Pointcut;
+
 @Aspect 
 public class LoggingAdvice {
  
-    @Pointcut("execution(* co.kr.YDCAT.*controller.*(..))")
-     public void logging() {    }
+    @Pointcut("execution(* co.kr.YDCAT.*.*(..))")
+     public void logging2() {};
       
-    @Before("logging()") 
-    public void before() {
+    @Before("logging2()") 
+    public void before(JoinPoint joinPoint) {
          System.out.println("* AOP ¼º°ø before *"); 
     } 
      
